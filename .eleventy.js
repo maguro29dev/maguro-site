2a0f0d7d566e85e0bfce0a1ee413e87a01b601b3
maguro29dev <cozy.1106@gmail.com>
2025-08-08 16:38:50 +0900

    YouTube自動更新機能を追加

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,19 @@
+// この1行をファイルの先頭に追加します
+require('dotenv').config();
+
 module.exports = function(eleventyConfig) {
+  // Nunjucksで日付をフォーマットするためのフィルターを追加
+  eleventyConfig.addFilter("date", function(dateObj, format) {
+    // luxon はこのフィルターの中で呼び出します
+    const { DateTime } = require("luxon");
+    // .setLocale('ja') を追加して、曜日などを日本語で表示します
+    return DateTime.fromJSDate(new Date(dateObj)).setZone('Asia/Tokyo').setLocale('ja').toFormat(format);
+  });
+
   return {
+    // .md ファイルを Nunjucks エンジンで処理するように設定します
+    markdownTemplateEngine: "njk",
+    
     dir: {
       input: "src",
       output: "_site",
